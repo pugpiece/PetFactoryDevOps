90479b16708529447e24d5f88c000bf35d2507f1
Олег <bruskovod@gmail.com>
2022-01-12 17:28:41 +0400

    test before showing 2

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -36,6 +36,8 @@ public class BaseEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	private Integer test;
+
 	public Integer getId() {
 		return id;
 	}
